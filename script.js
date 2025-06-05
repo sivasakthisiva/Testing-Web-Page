7fb2953a5a70da24a2e4cac5dd6e83848db08075
siva <144245670+sivasakthisiva@users.noreply.github.com>
2025-06-05 22:49:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const display = document.getElementById('display');
+
+function appendValue(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value.replace('%', '/100'));
+  } catch (e) {
+    display.value = 'Error';
+  }
+}
